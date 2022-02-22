b61a8b996e1793337f13806b9b6135aaf72251fe
maxiKachan <max101111427@gmail.com>
2022-02-22 14:15:38 +0300

    create authentication and registration service

---
+++ auth-reg-service/src/main/java/com/example/authregservice/AuthRegServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.authregservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthRegServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthRegServiceApplication.class, args);
+    }
+
+}
---
+++ auth-reg-service/src/test/java/com/example/authregservice/AuthRegServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authregservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthRegServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
