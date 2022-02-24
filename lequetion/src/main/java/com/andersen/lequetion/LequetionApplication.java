793809089f2f6648f778fb1c8ec536cea50529e3
maxiKachan <max101111427@gmail.com>
2022-02-24 12:36:26 +0300

    add liquetion module

---
+++ lequetion/src/main/java/com/andersen/lequetion/LequetionApplication.java
@@ -0,0 +1,13 @@
+package com.andersen.lequetion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LequetionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LequetionApplication.class, args);
+    }
+
+}
